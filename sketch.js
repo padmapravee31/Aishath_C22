0e474c576ccc475dab0fe3de3ef7f9c283641c3d
padmapravee31 <padmapravee31@gmail.com>
2020-09-25 19:38:53 +0530

    c22

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var options = {
+    isStatic: true
+  }
+  object = Bodies.rectangle(200,100,50,50,options);
+  World.add(world,object);
+
+  console.log(object);
+  
 }
 
 function draw() {
-  background(255,255,255);  
+  Engine.update(engine);
+  background("black"); 
+  rectMode(CENTER);
+  rect(object.position.x,object.position.y,50,50);
   drawSprites();
 }
